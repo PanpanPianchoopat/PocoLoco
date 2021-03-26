ab4f10b1193e742f861edf1394aa80de2337e12f
qbeamm <qbeamm@gmail.com>
2021-03-27 03:18:58 +0700

    B009-HotelExpenses

---
+++ src/views/HotelExpenses.vue
@@ -0,0 +1,25 @@
+<template>
+  <Navbar />
+  <Container>
+    <div>
+      <h1>Hotel Expenses</h1>
+
+    </div>
+  </Container>
+</template>
+
+<script>
+  import DefaultButton from "../components/DefaultButton.vue";
+  import Navbar from "../components/Navbar.vue";
+  import Container from "../components/Container.vue";
+  export default {
+    name: "HotelExpenses",
+    components: { DefaultButton, Navbar, Container },
+
+
+  };
+</script>
+
+<style>
+
+</style>
