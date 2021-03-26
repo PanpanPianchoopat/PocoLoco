03cd55dfe488056fc03a2c054f17984f711a97c0
qbeamm <qbeamm@gmail.com>
2021-03-27 03:22:54 +0700

    B011-MyAccount

---
+++ src/views/MyAccount.vue
@@ -0,0 +1,25 @@
+<template>
+  <Navbar />
+  <Container>
+    <div>
+      <h1>My Account</h1>
+
+    </div>
+  </Container>
+</template>
+
+<script>
+  import DefaultButton from "../components/DefaultButton.vue";
+  import Navbar from "../components/Navbar.vue";
+  import Container from "../components/Container.vue";
+  export default {
+    name: "MyAccount",
+    components: { DefaultButton, Navbar, Container },
+
+
+  };
+</script>
+
+<style>
+
+</style>
