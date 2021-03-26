f4fff6e7849affe2e9f86fda90155e236464cbda
qbeamm <qbeamm@gmail.com>
2021-03-26 16:34:13 +0700

    Put navbar in customer page

--- src/views/Customer.vue
+++ src/views/Customer.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-      <div>Customer</div>
-  </div>
+    <Navbar />
+    <Container>
+        <div>
+            <h1>Customer</h1>
+
+        </div>
+    </Container>
 </template>
 
 <script>
+  import DefaultButton from "../components/DefaultButton.vue";
+  import Navbar from "../components/Navbar.vue";
+  import Container from "../components/Container.vue";
 export default {
+  components: { DefaultButton, Navbar, Container },
 
 }
 </script>
