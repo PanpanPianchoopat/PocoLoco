a5d19bc9de6274271651763865bf99ea15156f65
qbeamm <qbeamm@gmail.com>
2021-03-26 18:12:17 +0700

    Room Service

---
+++ src/views/RoomService.vue
@@ -0,0 +1,23 @@
+<template>
+    <Navbar />
+    <Container>
+        <div>
+            <h1>Room Service</h1>
+
+        </div>
+    </Container>
+</template>
+
+<script>
+  import DefaultButton from "../components/DefaultButton.vue";
+  import Navbar from "../components/Navbar.vue";
+  import Container from "../components/Container.vue";
+export default {
+  components: { DefaultButton, Navbar, Container },
+
+}
+</script>
+
+<style>
+
+</style>
