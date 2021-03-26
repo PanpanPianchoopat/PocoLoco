028d30d6601a9b856830c4dcbabf002a5deffd5f
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2021-03-26 17:03:46 +0700

    Create add button component

---
+++ src/components/AddButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="add-button">
+    <span style="color: white;">
+      <i class="fa fa-plus fa-3x"></i>
+    </span>
+  </button>
+</template>
+
+<script>
+  export default {
+    name: "AddButton",
+  };
+</script>
+
+<style>
+  .add-button {
+    width: 60px;
+    height: 60px;
+    border-radius: 50px;
+    background: #54cfd6;
+    outline: none;
+    border: none;
+  }
+  .add-button:hover {
+    cursor: pointer;
+    opacity: 0.7;
+  }
+</style>
