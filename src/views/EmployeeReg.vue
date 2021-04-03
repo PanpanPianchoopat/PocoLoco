6715f74580fd8ce0ec2c4b90e093d4f4000a2581
yingspvd <supavadeeying@outlook.com>
2021-04-03 14:55:43 +0700

    Y001 - EmployeeReg

--- src/views/EmployeeReg.vue
+++ src/views/EmployeeReg.vue
@@ -7,8 +7,14 @@
       </div>
 
       <InnerFormContainer>
+        <!-- show data -->
+        <div class="py-2">
+          {{ details }}
+        </div>
+
         <div class="input-group">
           <div>
+            <!-- Department -->
             <h4>Department</h4>
             <select @change="getRole" v-model="details.department">
               <option disabled value="">Department</option>
@@ -21,31 +27,34 @@
               <option value="7">Maintenance</option>
             </select>
           </div>
+
+          <!-- Role -->
           <div>
             <h4>Role</h4>
             <select v-model="details.roleID">
-              <option value="">123</option>
-              <option
-                :key="item.id"
-                v-bind:value="item.roleID"
-                v-for="item in roleDB"
-              >
-                {{ item.roleName }}
-              </option>
               <!-- <option disabled value>Role</option>
               <option v-for="item in roleDB" 
               :value="item.roleID" 
               :key="item">
                 {{ item.roleName }}
               </option> -->
+              <option disabled value>Role</option>
+              <option value="10">Admin</option>
+              <option value="20">Owner</option>
+              <option value="30">Receptionist</option>
+              <option value="40">Manager</option>
+              <option value="50">Chef</option>
+              <option value="60">Maid</option>
+              <option value="70">Staff</option>
             </select>
           </div>
         </div>
 
+        <!-- Start Date -->
         <h4>Start Date</h4>
         <div class="flex x-full">
           <v-date-picker
-            v-model="startDate"
+            v-model="details.startDate"
             :masks="{ input: ['YYYY-MM-DD'] }"
             mode="single"
             class="flex-grow"
@@ -62,28 +71,57 @@
             </template>
           </v-date-picker>
         </div>
+
+        <!-- Shift -->
+        <div>
+          <h4>Shift</h4>
+          <select v-model="details.shift">
+            <option disabled value>Shift</option>
+            <option value="1">05.00 - 13.00</option>
+            <option value="2">13.00 - 21.00</option>
+            <option value="3">21.00 - 05.00</option>
+          </select>
+        </div>
+
         <hr />
 
+        <!-- First Name -->
         <div class="input-group">
           <div>
             <h4>First Name</h4>
-            <input type="text" placeholder="ex. Poco" />
+            <input
+              type="text"
+              v-model="details.firstName"
+              placeholder="ex. Poco"
+            />
           </div>
+
+          <!-- Lastname -->
           <div>
             <h4>Last Name</h4>
-            <input type="text" placeholder="ex. Loco" />
+            <input
+              type="text"
+              v-model="details.lastName"
+              placeholder="ex. Loco"
+            />
           </div>
         </div>
 
+        <!-- Identification -->
         <h4>ID Number / Passport Number</h4>
-        <input type="text" />
+        <input
+          type="text"
+          v-model="details.identification"
+          pattern="^[A-PR-WYa-pr-wy][1-9]\d\s?\d{4}[1-9]$"
+        />
 
         <div class="input-group">
           <div>
+            <!-- DOB -->
             <h4>Date of Birth</h4>
             <div class="flex x-full">
               <v-date-picker
-                v-model="birthDate"
+                v-model="details.DOB"
                 :masks="{ input: ['YYYY-MM-DD'] }"
                 mode="single"
                 class="flex-group"
@@ -102,25 +140,51 @@
             </div>
           </div>
           <div>
+            <!-- Gender -->
             <h4>Gender</h4>
-            <select></select>
+            <select v-model="details.gender">
+              <option disabled value>Gender</option>
+              <option value="M">Male</option>
+              <option value="F">Female</option>
+            </select>
           </div>
         </div>
 
+        <!-- Phone -->
         <h4>Phone</h4>
-        <input type="text" placeholder="ex. 0812345678" />
+        <input
+          type="text"
+          v-model="details.phone"
+          placeholder="ex. 0812345678"
+        />
 
+        <!-- Email -->
         <h4>Email</h4>
-        <input type="text" placeholder="ex. employee@mail.com" />
+        <input
+          type="text"
+          v-model="details.email"
+          placeholder="ex. employee@mail.com"
+        />
 
         <div class="input-group">
+          <!-- Password -->
           <div>
             <h4>Password</h4>
-            <input type="password" placeholder="Password" />
+            <input
+              type="password"
+              v-model="details.password"
+              placeholder="Password"
+            />
           </div>
+
+          <!-- Confirm Password -->
           <div>
             <h4>Confirm Password</h4>
-            <input type="password" placeholder="Password" />
+            <input
+              type="password"
+              v-model="details.cf_pass"
+              placeholder="Password"
+            />
           </div>
         </div>
       </InnerFormContainer>
@@ -133,7 +197,9 @@
           }"
           >CANCEL</DefaultButton
         >
-        <DefaultButton :style="{ background: '#54CFD6' }">ADD</DefaultButton>
+        <DefaultButton :style="{ background: '#54CFD6' }" @click="addEmployee"
+          >ADD</DefaultButton
+        >
       </div>
     </FormContainer>
   </div>
@@ -176,9 +242,20 @@ export default {
   },
 
   methods: {
+    fetch() {
+      axios
+        .get("http://localhost:8080/PocoLoco_db/api_addEmployee.php")
+        .then((response) => {
+          console.log("SUCCESSS");
+        })
+        .catch(() => {
+          console.log("ERROR");
+        });
+    },
+
     getRole() {
       axios
-        .post("http://localhost:8080/vue_php/api_addEmployee.php", {
+        .post("http://localhost:8080/PocoLoco_db/api_addEmployee.php", {
           action: "getRole",
           department: this.details.department,
         })
@@ -187,14 +264,29 @@ export default {
           console.log(app.roleDB);
         });
     },
-    fetch() {
+
+    addEmployee(e) {
+      e.preventDefault();
       axios
-        .get("http://localhost:8080/vue_php/api_addEmployee.php")
-        .then((response) => {
-          console.log("SUCCESSS");
+        .post("http://localhost:8080/PocoLoco_db/api_addEmployee.php", {
+          action: "addEmployee",
+          department: this.details.department,
+          roleID: this.details.roleID,
+          shift: this.details.shift,
+          startDate: this.details.startDate,
+          firstName: this.details.firstName,
+          lastName: this.details.lastName,
+          identification: this.details.identification,
+          DOB: this.details.DOB,
+          gender: this.details.gender,
+          phone: this.details.phone,
+          email: this.details.email,
+          password: this.details.password,
+          cf_pass: this.details.cf_pass,
         })
-        .catch(() => {
-          console.log("ERROR");
+        .then(function(res) {
+          console.log(res.data);
+          //alert(res.data);
         });
     },
   },
