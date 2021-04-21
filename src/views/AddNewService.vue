69831ebda9331a51256ddbcf920bb87e90067919
yingspvd <supavadeeying@outlook.com>
2021-04-22 00:52:37 +0700

    AddNewService

--- src/views/AddNewService.vue
+++ src/views/AddNewService.vue
@@ -5,18 +5,58 @@
     </div>
 
     <InnerFormContainer>
-      <h4>Service Type</h4>
-      <select></select>
+      <!-- Service Type Error -->
+      <div v-if="serviceTypeError">
+        <h4 style="color:red">Service Type</h4>
+        <select v-model="serviceType">
+          <option disabled value="">Service Type</option>
+          <option value="1">Food</option>
+          <option value="2">Facilities</option>
+        </select>
+      </div>
+
+      <!-- Service Type -->
+      <div v-else>
+        <h4>Service Type</h4>
+        <select v-model="serviceType">
+          <option disabled value="">Service Type</option>
+          <option value="1">Food</option>
+          <option value="2">Facilities</option>
+        </select>
+      </div>
 
       <div class="input-group">
-        <div>
-          <h4>Service Name</h4>
-          <input type="text" />
+        <!-- Service Name Error -->
+        <div v-if="serviceNameError">
+          <h4 style="color:red">Service Name</h4>
+          <input v-model="serviceName" type="text" />
         </div>
-        <div>
+
+        <!-- Service Name -->
+        <div v-else>
+          <h4>Service Name</h4>
+          <input v-model="serviceName" type="text" />
+        </div>
+
+        <!-- Price Error -->
+        <div v-if="priceError">
+          <h4 style="color:red">Price</h4>
+          <div :style="{ display: 'flex', flexDirection: 'row' }">
+            <input
+              v-model="price"
+              type="number"
+              :style="{ marginRight: '10px', width: '200px' }"
+            />
+            <p class="unit">Baht</p>
+          </div>
+        </div>
+
+        <!-- Price -->
+        <div v-else>
           <h4>Price</h4>
           <div :style="{ display: 'flex', flexDirection: 'row' }">
             <input
+              v-model="price"
               type="number"
               :style="{ marginRight: '10px', width: '200px' }"
             />
@@ -34,70 +74,143 @@
         }"
         >CANCEL</DefaultButton
       >
-      <DefaultButton :style="{ background: '#54CFD6' }">ADD</DefaultButton>
+      <DefaultButton :style="{ background: '#54CFD6' }" @click="addNewService"
+        >ADD</DefaultButton
+      >
     </div>
   </FormContainer>
 </template>
 
 <script>
-  import FormContainer from "../components/FormContainer.vue";
-  import DefaultButton from "../components/DefaultButton.vue";
-  import InnerFormContainer from "../components/InnerFormContainer.vue";
-  export default {
-    name: "AddNewService",
-    components: { FormContainer, DefaultButton, InnerFormContainer },
-    data() {
-      return {};
+import FormContainer from "../components/FormContainer.vue";
+import DefaultButton from "../components/DefaultButton.vue";
+import InnerFormContainer from "../components/InnerFormContainer.vue";
+import axios from "axios";
+
+export default {
+  name: "AddNewService",
+  components: { FormContainer, DefaultButton, InnerFormContainer },
+  data() {
+    return {
+      serviceType: "",
+      serviceName: "",
+      price: "",
+
+      check: false,
+      serviceTypeError: false,
+      serviceNameError: false,
+      priceError: false,
+    };
+  },
+
+  methods: {
+    addNewService(e) {
+      e.preventDefault();
+      this.validate();
+      if (this.check) {
+        axios
+          .post("http://localhost:8080/PocoLoco_db/api_addNewService.php", {
+            action: "addNewService",
+            serviceType: this.serviceType,
+            serviceName: this.serviceName,
+            price: this.price,
+          })
+          .then(
+            function(res) {
+              console.log(res);
+              if (res.data.success == true) {
+                alert("Saved Successful");
+                this.resetData();
+              } else {
+                this.message = res.data.message;
+              }
+            }.bind(this)
+          );
+      }
     },
-  };
+
+    validate() {
+      this.check =
+        this.serviceType != "" && this.serviceName != "" && this.price != "";
+
+      if (this.serviceType == "") {
+        this.serviceTypeError = true;
+      }
+      if (this.serviceName == "") {
+        this.serviceNameError = true;
+      }
+      if (this.price == "") {
+        this.priceError = true;
+      }
+      if (this.serviceType != "") {
+        this.serviceTypeError = false;
+      }
+      if (this.serviceName != "") {
+        this.serviceNameError = false;
+      }
+      if (this.price != "") {
+        this.priceError = false;
+      }
+    },
+
+    resetData() {
+      this.serviceType = "";
+      this.serviceName = "";
+      this.price = "";
+      this.serviceTypeError = false;
+      this.serviceNameError = false;
+      this.priceError = false;
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .form-header {
-    padding: 90px 0 10px 10%;
-  }
-  h2 {
-    color: white;
-    font-size: 36px;
-    margin-bottom: 10px;
-  }
+.form-header {
+  padding: 90px 0 10px 10%;
+}
+h2 {
+  color: white;
+  font-size: 36px;
+  margin-bottom: 10px;
+}
+.input-group {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+}
+input {
+  width: 300px;
+  height: 35px;
+  margin: 0 80px 40px 0;
+  padding-left: 10px;
+}
+select {
+  width: 220px;
+  height: 40px;
+  margin: 0 180px 30px 0;
+  padding-left: 10px;
+}
+.buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 45px 0;
+}
+i {
+  color: #0a96b7;
+  margin: 5px 0 0 -35px;
+  padding-right: 240px;
+}
+*:focus {
+  outline: 0;
+}
+@media (max-width: 1000px) {
   .input-group {
-    width: 100%;
-    display: flex;
-    flex-direction: row;
+    flex-direction: column;
   }
-  input {
-    width: 300px;
-    height: 35px;
-    margin: 0 80px 40px 0;
-    padding-left: 10px;
+  .form-header {
+    padding: 20px 0 10px 10%;
   }
-  select {
-    width: 220px;
-    height: 40px;
-    margin: 0 180px 30px 0;
-    padding-left: 10px;
-  }
-  .buttons {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    margin: 45px 0;
-  }
-  i {
-    color: #0a96b7;
-    margin: 5px 0 0 -35px;
-    padding-right: 240px;
-  }
-  *:focus {
-    outline: 0;
-  }
-  @media (max-width: 1000px) {
-    .input-group {
-      flex-direction: column;
-    }
-    .form-header {
-      padding: 20px 0 10px 10%;
-    }
-  }
+}
 </style>
