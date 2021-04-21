2d5a63093492b83b7443a3302b1c8a93ff2767db
yingspvd <supavadeeying@outlook.com>
2021-04-21 23:39:14 +0700

    NewService

---
+++ src/views/AddNewService.vue
@@ -0,0 +1,103 @@
+<template>
+  <FormContainer>
+    <div class="form-header">
+      <h2>Add New Service</h2>
+    </div>
+
+    <InnerFormContainer>
+      <h4>Service Type</h4>
+      <select></select>
+
+      <div class="input-group">
+        <div>
+          <h4>Service Name</h4>
+          <input type="text" />
+        </div>
+        <div>
+          <h4>Price</h4>
+          <div :style="{ display: 'flex', flexDirection: 'row' }">
+            <input
+              type="number"
+              :style="{ marginRight: '10px', width: '200px' }"
+            />
+            <p class="unit">Baht</p>
+          </div>
+        </div>
+      </div>
+    </InnerFormContainer>
+    <div class="buttons">
+      <DefaultButton
+        :style="{
+          background: 'none',
+          marginRight: '110px',
+          border: '3px solid white',
+        }"
+        >CANCEL</DefaultButton
+      >
+      <DefaultButton :style="{ background: '#54CFD6' }">ADD</DefaultButton>
+    </div>
+  </FormContainer>
+</template>
+
+<script>
+  import FormContainer from "../components/FormContainer.vue";
+  import DefaultButton from "../components/DefaultButton.vue";
+  import InnerFormContainer from "../components/InnerFormContainer.vue";
+  export default {
+    name: "AddNewService",
+    components: { FormContainer, DefaultButton, InnerFormContainer },
+    data() {
+      return {};
+    },
+  };
+</script>
+
+<style scoped>
+  .form-header {
+    padding: 90px 0 10px 10%;
+  }
+  h2 {
+    color: white;
+    font-size: 36px;
+    margin-bottom: 10px;
+  }
+  .input-group {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+  }
+  input {
+    width: 300px;
+    height: 35px;
+    margin: 0 80px 40px 0;
+    padding-left: 10px;
+  }
+  select {
+    width: 220px;
+    height: 40px;
+    margin: 0 180px 30px 0;
+    padding-left: 10px;
+  }
+  .buttons {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    margin: 45px 0;
+  }
+  i {
+    color: #0a96b7;
+    margin: 5px 0 0 -35px;
+    padding-right: 240px;
+  }
+  *:focus {
+    outline: 0;
+  }
+  @media (max-width: 1000px) {
+    .input-group {
+      flex-direction: column;
+    }
+    .form-header {
+      padding: 20px 0 10px 10%;
+    }
+  }
+</style>
