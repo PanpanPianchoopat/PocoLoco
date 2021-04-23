33938b4176a03065d507329c1e951967177b1001
yingspvd <supavadeeying@outlook.com>
2021-04-24 01:38:09 +0700

    vuex package

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VCalendar from "v-calendar";
+import store from './store'
 
-createApp(App)
+createApp(App).use(store)
   .use(router)
   .use(VCalendar, {})
   .mount("#app");
