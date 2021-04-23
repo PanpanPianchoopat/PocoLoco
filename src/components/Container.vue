6149e23863c9d927f8a444f944d46fd2d5defc8e
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2021-04-23 15:55:07 +0700

    Edit container

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -15,11 +15,16 @@
     position: fixed;
     display: flex;
     flex-direction: column;
-    width: 100%;
+    width: 100vw;
     height: 100%;
-    margin: 0 60px 0 200px;
-    padding: 0 60px;
+    margin: 0 auto;
+    padding: 0 100px;
     flex-wrap: wrap;
     background: #f8f8f8;
+  }
+  @media (max-width: 700px) {
+    .container {
+      padding: 0 80px;
+    }
   }
 </style>
