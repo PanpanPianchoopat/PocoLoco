d0258fbe6637eb504c53d12a1fa2fb24f86b437c
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2021-04-24 17:38:28 +0700

    Edit constant

--- src/composables/useScreenHeight.js
+++ src/composables/useScreenHeight.js
@@ -8,7 +8,7 @@ export const useScreenHeight = () => {
   onUnmounted(() => window.removeEventListener("resize", onHeightChange));
 
   const height = computed(() => windowHeight.value);
-  const tableRow = computed(() => Math.floor((windowHeight.value - 430) / 35));
+  const tableRow = computed(() => Math.floor((windowHeight.value - 440) / 35));
 
   return { height, tableRow };
 };
