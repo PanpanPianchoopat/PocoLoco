9a051d3599d906a13dfe40160c461c61f00dbf58
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2021-04-29 09:47:43 +0700

    Create card component

---
+++ src/components/RoomCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="card-container"
+    :style="type == 'active' ? { border: '2px solid black' } : {}"
+  >
+    <b><slot></slot></b>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "RoomCard",
+    props: ["type"],
+  };
+</script>
+
+<style scoped>
+  .card-container {
+    width: 95px;
+    height: 95px;
+    display: flex;
+    background: #d6e7ea;
+    justify-content: center;
+  }
+  b {
+    margin-top: 10px;
+  }
+</style>
