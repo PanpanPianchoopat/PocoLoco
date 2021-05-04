df030e26204abb0c5ca5a86ba118cde836ea9b0d
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2021-05-04 22:28:23 +0700

    Search error and use case example in customer page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,12 @@
   <Navbar />
   <Container>
     <h1>Home Page</h1>
+
     <button @click="visible = !visible">Show Popup</button>
-    <Popup v-bind:visible="visible" @popReturn = "popReturn">
+    <Popup v-bind:visible="visible" @popReturn="popReturn">
       <h4>Service Type</h4>
     </Popup>
+    <SearchError />
   </Container>
 </template>
 
@@ -13,13 +15,15 @@
   import Navbar from "../components/Navbar.vue";
   import Container from "../components/Container.vue";
   import Popup from "../components/Popup.vue";
+  import SearchError from "../components/SearchError";
+
   export default {
     name: "Home",
-    components: { Navbar, Container, Popup },
+    components: { Navbar, Container, Popup, SearchError },
     methods: {
-      popReturn(value){
-        this.visible = value
-      }
+      popReturn(value) {
+        this.visible = value;
+      },
     },
     data() {
       return {
