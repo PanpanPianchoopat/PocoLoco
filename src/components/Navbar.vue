fb9fbfcb544eba52d37ca868020ea81cc88d386f
yingspvd <supavadeeying@outlook.com>
2021-05-05 14:17:09 +0700

    Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -29,6 +29,8 @@
       <router-link :to="{ name: 'About' }">About</router-link>
       <router-link :to="{ name: 'Role' }">Role</router-link>
       <router-link :to="{ name: 'Customer' }">Customer</router-link>
+      <router-link :to="{ name: 'HotelExpenses' }">Hotel Expenses</router-link>
+      <router-link :to="{ name: 'Booking' }">Booking</router-link>
     </div>
     <button class="logout-button" v-if="visible || width > 1000">
       <div class="logout-text">
@@ -44,137 +46,137 @@
 </template>
 
 <script>
-  import { useScreenWidth } from "../composables/useScreenWidth";
-  export default {
-    name: "Navbar",
-    setup() {
-      const { width } = useScreenWidth();
-      return { width };
+import { useScreenWidth } from "../composables/useScreenWidth";
+export default {
+  name: "Navbar",
+  setup() {
+    const { width } = useScreenWidth();
+    return { width };
+  },
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  methods: {
+    returnVisible() {
+      this.visible = !this.visible;
+      this.$emit("NavReturn", this.visible);
     },
-    data() {
-      return {
-        visible: false,
-      };
-    },
-    methods: {
-      returnVisible() {
-        this.visible = !this.visible;
-        this.$emit("NavReturn", this.visible);
-      },
-    },
-  };
+  },
+};
 </script>
 
 <style scoped>
-  #nav {
-    width: 200px;
-    height: 100%;
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    overflow-x: hidden;
-    display: flex;
-    padding-top: 60px;
-    flex-direction: column;
-    background-color: var(--primary-blue);
-    transition: width 0.25s;
-  }
-  #nav a {
-    font-weight: bold;
-    color: #fcfcfc;
-    text-decoration: none;
-    font-size: 18px;
-    padding: 10px 0;
-    padding-left: 40px;
-  }
-  #nav a:hover {
-    background: white;
-    border-radius: 50px;
-    opacity: 0.85;
-    z-index: 10;
-    color: var(--text-color);
-  }
-  #nav a.router-link-exact-active {
-    color: var(--text-color);
-    background: white;
-    border-radius: 50px;
-    z-index: 10;
-  }
-  .close {
-    width: 30px;
-    height: 30px;
-    background: none;
-    border: none;
-    position: fixed;
-    top: 5px;
-    left: 165px;
-    z-index: 10;
-    cursor: pointer;
-  }
-  .close:hover {
-    opacity: 0.7;
-  }
-  i {
-    color: white;
-    cursor: pointer;
-  }
-  .circle {
-    width: 90px;
-    height: 90px;
-    background: var(--primary-yellow);
-    display: flex;
-    justify-content: center;
-    align-self: center;
-    border-radius: 50%;
-    overflow: hidden;
-  }
-  img {
-    width: 55px;
-    align-self: center;
-    padding-top: 25px;
-  }
-  .info {
-    display: flex;
-    flex-direction: column;
-    padding: 10px 0;
-  }
-  b {
-    font-size: 18px;
-    color: white;
-    align-self: center;
-  }
-  .menu {
-    display: flex;
-    flex-direction: column;
-    padding: 45px 0 0 20px;
-    margin-right: -20px;
-  }
-  .logout-button {
-    width: 120px;
-    left: 40px;
-    bottom: 30px;
-    height: 35px;
-    position: fixed;
-    z-index: 10;
-    background: none;
-    border: none;
-  }
-  .logout-button:hover {
-    cursor: pointer;
-    opacity: 0.7;
-  }
-  .logout-text {
-    width: 100%;
-    color: white;
-    font-size: 16px;
-    font-family: sans-serif;
-    font-weight: bold;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-  *:focus {
-    outline: 0;
-  }
+#nav {
+  width: 200px;
+  height: 100%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  overflow-x: hidden;
+  display: flex;
+  padding-top: 60px;
+  flex-direction: column;
+  background-color: var(--primary-blue);
+  transition: width 0.25s;
+}
+#nav a {
+  font-weight: bold;
+  color: #fcfcfc;
+  text-decoration: none;
+  font-size: 18px;
+  padding: 10px 0;
+  padding-left: 40px;
+}
+#nav a:hover {
+  background: white;
+  border-radius: 50px;
+  opacity: 0.85;
+  z-index: 10;
+  color: var(--text-color);
+}
+#nav a.router-link-exact-active {
+  color: var(--text-color);
+  background: white;
+  border-radius: 50px;
+  z-index: 10;
+}
+.close {
+  width: 30px;
+  height: 30px;
+  background: none;
+  border: none;
+  position: fixed;
+  top: 5px;
+  left: 165px;
+  z-index: 10;
+  cursor: pointer;
+}
+.close:hover {
+  opacity: 0.7;
+}
+i {
+  color: white;
+  cursor: pointer;
+}
+.circle {
+  width: 90px;
+  height: 90px;
+  background: var(--primary-yellow);
+  display: flex;
+  justify-content: center;
+  align-self: center;
+  border-radius: 50%;
+  overflow: hidden;
+}
+img {
+  width: 55px;
+  align-self: center;
+  padding-top: 25px;
+}
+.info {
+  display: flex;
+  flex-direction: column;
+  padding: 10px 0;
+}
+b {
+  font-size: 18px;
+  color: white;
+  align-self: center;
+}
+.menu {
+  display: flex;
+  flex-direction: column;
+  padding: 45px 0 0 20px;
+  margin-right: -20px;
+}
+.logout-button {
+  width: 120px;
+  left: 40px;
+  bottom: 30px;
+  height: 35px;
+  position: fixed;
+  z-index: 10;
+  background: none;
+  border: none;
+}
+.logout-button:hover {
+  cursor: pointer;
+  opacity: 0.7;
+}
+.logout-text {
+  width: 100%;
+  color: white;
+  font-size: 16px;
+  font-family: sans-serif;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+*:focus {
+  outline: 0;
+}
 </style>
