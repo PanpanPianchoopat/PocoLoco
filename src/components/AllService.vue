ab740ddc2482ecd82f8c45213b6920a217d2e773
yingspvd <supavadeeying@outlook.com>
2021-05-08 23:26:22 +0700

    All Service

--- src/components/AllService.vue
+++ src/components/AllService.vue
@@ -4,6 +4,7 @@
       <i class="fa fa-search fa-1x"></i>
 
       <input
+        v-model="search"
         class="search-field"
         type="text"
         placeholder="search"
@@ -13,17 +14,17 @@
     <div class="menu-buttons">
       <CustomSelect
         type="Filter"
-        :options="['A', 'B', 'C', 'D']"
+        :options="selectOption"
         :style="{ marginRight: '20px' }"
         @selection="selectionFilter"
       />
       <CustomSelect
         type="Sort by"
-        :options="['A', 'B', 'C']"
+        :options="selectOption"
         :style="{ marginRight: '20px' }"
         @selection="selectionSort"
       />
-      <DefaultButton type="small">
+      <DefaultButton type="small" @click="searchService">
         Search
       </DefaultButton>
     </div>
@@ -32,8 +33,9 @@
       :style="{ position: 'absolute', right: '2%' }"
     />
   </div>
+  <SearchError v-if="errorSearching" />
   <div class="table-container">
-    <table v-if="services.length !== 0">
+    <table v-if="service_db.length !== 0">
       <tr>
         <th>Service Name</th>
         <th>Type</th>
@@ -42,7 +44,7 @@
       </tr>
 
       <tr
-        v-for="(service, i) in services.slice(
+        v-for="(service, i) in service_db.slice(
           currentPage * tableRow - tableRow,
           currentPage * tableRow
         )"
@@ -50,18 +52,18 @@
         class="row"
       >
         <td>
-          {{ service.serviceName }}
+          {{ service.name }}
         </td>
         <td>{{ service.type }}</td>
-        <td>{{ service.price }}</td>
+        <td>{{ service.servicePrice }}</td>
 
         <td>
           <div class="manage">
-            <button class="manage-button" @click="showPopup(service)">
+            <button class="manage-button" @click="getServiceEdit(service)">
               <i class="fa fa-pencil fa-2x"></i>
             </button>
             <div class="vl"></div>
-            <button class="manage-button">
+            <button class="manage-button" @click="deleteService(service)">
               <i class="fa fa-trash fa-2x"></i>
             </button>
           </div>
@@ -71,8 +73,8 @@
   </div>
 
   <PaginationBar
-    :pageCount="Math.ceil(services.length / tableRow)"
-    :paginationVisible="services.length > tableRow"
+    :pageCount="Math.ceil(service_db.length / tableRow)"
+    :paginationVisible="service_db.length > tableRow"
     @pageReturn="pageReturn"
   />
 
@@ -80,18 +82,19 @@
     v-bind:visible="editVisible"
     :buttons="true"
     @popReturn="editReturn"
+    @submit="submit"
     :style="{ top: '0', left: '0', margin: '0' }"
   >
     <h4>Service Type</h4>
-    <select>
-      <option :value="serviceType" selected disabled hidden>
-        {{ serviceType }}
+    <select v-model="form.type">
+      <option :value="form.type" selected disabled hidden>
+        {{ form.type }}
       </option>
       <option
         v-for="(option, i) in serviceOptions"
         :key="i"
-        :value="serviceType"
-        :selected="option == serviceType ? 'selected' : null"
+        :value="serviceOptions[i]"
+        :selected="option == form.type ? 'selected' : null"
       >
         {{ option }}
       </option>
@@ -99,14 +102,14 @@
     <h4>Service Name</h4>
     <input
       type="text"
-      :value="serviceName"
+      v-model="form.name"
       :placeholder="serviceName"
       :style="{ width: '280px' }"
     />
     <h4>Price</h4>
     <div class="input-group">
       <input
-        :value="servicePrice"
+        v-model="form.servicePrice"
         :placeholder="servicePrice"
         :style="{ marginRight: '10px', textAlign: 'right' }"
       />
@@ -116,256 +119,339 @@
 </template>
 
 <script>
-  import CustomSelect from "./CustomSelect";
-  import DefaultButton from "./DefaultButton";
-  import AddButton from "./AddButton";
-  import { useScreenWidth } from "../composables/useScreenWidth";
-  import { useScreenHeight } from "../composables/useScreenHeight";
-  import PaginationBar from "./PaginationBar";
-  import Popup from "../components/Popup";
-  const services = [
-    {
-      type: "Room Facility",
-      serviceName: "Extra Blanket",
-      price: 350,
-    },
-    {
-      type: "Room Facility",
-      serviceName: "Extra Pillow",
-      price: 250,
-    },
-    {
-      type: "Room Facility",
-      serviceName: "Extra Towel",
-      price: 150,
-    },
-    {
-      type: "Food & Bev",
-      serviceName: "American Breakfast Set",
-      price: 250,
-    },
-    {
-      type: "Food & Bev",
-      serviceName: "Fresh Orange Juice",
-      price: 80,
-    },
-    {
-      type: "Food & Bev",
-      serviceName: "Crab Fried Rice",
-      price: 180,
-    },
-    {
-      type: "Food & Bev",
-      serviceName: "Crab Fried Rice",
-      price: 180,
-    },
-    {
-      type: "Food & Bev",
-      serviceName: "Crab Fried Rice",
-      price: 180,
-    },
-    {
-      type: "Food & Bev",
-      serviceName: "Crab Fried Rice",
-      price: 180,
-    },
-    {
-      type: "Food & Bev",
-      serviceName: "Crab Fried Rice",
-      price: 180,
-    },
-    {
-      type: "Food & Bev",
-      serviceName: "Crab Fried Rice",
-      price: 180,
-    },
-    {
-      type: "Food & Bev",
-      serviceName: "Crab Fried Rice",
-      price: 180,
-    },
-    {
-      type: "Food & Bev",
-      serviceName: "Crab Fried Rice",
-      price: 180,
-    },
-  ];
-  export default {
-    name: "AllService",
-    components: {
-      CustomSelect,
-      DefaultButton,
-      AddButton,
-      PaginationBar,
-      Popup,
-    },
-    setup() {
-      const { width } = useScreenWidth();
-      return { width };
-    },
-    data() {
-      return {
-        services,
-        serviceOptions: ["Room Facility", "Food & Bev"],
-        currentPage: 1,
-        tableRow: 10,
-        editVisible: false,
-        serviceType: null,
-        serviceName: null,
-        servicePrice: null,
-      };
-    },
-    methods: {
-      pageReturn(page) {
-        this.currentPage = page;
+import CustomSelect from "./CustomSelect";
+import DefaultButton from "./DefaultButton";
+import AddButton from "./AddButton";
+import { useScreenWidth } from "../composables/useScreenWidth";
+import { useScreenHeight } from "../composables/useScreenHeight";
+import PaginationBar from "./PaginationBar";
+import Popup from "../components/Popup";
+import SearchError from "../components/SearchError";
+import axios from "axios";
+
+const selectOption = ["All", "Service Type", "Service Name", "Price"];
+
+export default {
+  name: "AllService",
+  components: {
+    CustomSelect,
+    DefaultButton,
+    AddButton,
+    PaginationBar,
+    Popup,
+    SearchError,
+  },
+  setup() {
+    const { width } = useScreenWidth();
+    return { width };
+  },
+  data() {
+    return {
+      currentPage: 1,
+      tableRow: 10,
+      editVisible: false,
+      selectOption,
+      serviceOptions: ["Room Facility", "Food & Beverage"],
+      service_db: "",
+      type_db: "",
+      search: "",
+      filter: "all",
+      sort: "all",
+      errorSearching: false,
+      check: false,
+      form: {
+        serviceID: "",
+        type: "",
+        name: "",
+        servicePrice: "",
+        isEdit: false,
       },
-      goToAddService() {
-        this.$router.push("/AddNewService");
-      },
-      editReturn(value) {
-        this.editVisible = value;
-      },
-      showPopup(service) {
-        this.serviceType = service.type;
-        this.serviceName = service.serviceName;
-        this.servicePrice = service.price;
-        this.editVisible = !this.editVisible;
-      },
+    };
+  },
+  created() {
+    this.getAllService();
+  },
+  methods: {
+    pageReturn(page) {
+      this.currentPage = page;
     },
-  };
+    goToAddService() {
+      this.$router.push("/AddNewService");
+    },
+    editReturn(value) {
+      this.editVisible = value;
+    },
+    submit(value) {
+      this.editVisible = value;
+      this.updateData();
+    },
+
+    getAllService() {
+      axios
+        .post("http://localhost:8080/PocoLoco_db/api_allService.php", {
+          action: "getAllService",
+        })
+        .then(
+          function(res) {
+            this.service_db = res.data;
+          }.bind(this)
+        );
+    },
+    updateData() {
+      this.serviceOption(this.form.type);
+      this.validate();
+      if (this.check) {
+        axios
+          .post("http://localhost:8080/PocoLoco_db/api_allService.php", {
+            action: "updateData",
+            serviceID: this.form.serviceID,
+            type: this.form.type,
+            name: this.form.name,
+            servicePrice: this.form.servicePrice,
+          })
+          .then(
+            function(res) {
+              if (res.data.success == true) {
+                alert(res.data.message);
+                this.resetData();
+                this.getAllService();
+              } else {
+                alert(res.data.message);
+              }
+            }.bind(this)
+          );
+      }
+    },
+    deleteService(service) {
+      if (confirm("Delete '" + service.name + "' ?")) {
+        axios
+          .post("http://localhost:8080/PocoLoco_db/api_allService.php", {
+            action: "deleteService",
+            serviceID: service.serviceID,
+          })
+          .then(
+            function(res) {
+              if (res.data.success == true) {
+                alert(res.data.message);
+                this.getAllService();
+              }
+            }.bind(this)
+          );
+      }
+    },
+    searchService() {
+      axios
+        .post("http://localhost:8080/PocoLoco_db/api_allService.php", {
+          action: "searchService",
+          search: this.search,
+          filter: this.filter,
+          sort: this.sort,
+        })
+        .then(
+          function(res) {
+            console.log(res);
+            this.service_db = res.data;
+            if (this.service_db != "") {
+              this.errorSearching = false;
+            } else {
+              this.errorSearching = true;
+            }
+          }.bind(this)
+        );
+    },
+    validate() {
+      this.check =
+        this.form.type != "" &&
+        this.form.name != "" &&
+        this.form.servicePrice != "";
+    },
+    resetData() {
+      this.form.serviceID = "";
+      this.form.type = "";
+      this.form.name = "";
+      this.form.servicePrice = "";
+      this.search = "";
+      this.filter = "";
+      this.sort = "";
+    },
+    selectionSort(value) {
+      if (value === selectOption[0]) {
+        this.sort = "all";
+      }
+      if (value === selectOption[1]) {
+        this.sort = "type";
+      }
+      if (value === selectOption[2]) {
+        this.sort = "name";
+      }
+      if (value === selectOption[3]) {
+        this.sort = "servicePrice";
+      }
+    },
+    selectionFilter(value) {
+      if (value === selectOption[0]) {
+        this.filter = "all";
+      }
+      if (value === selectOption[1]) {
+        this.filter = "type";
+      }
+      if (value === selectOption[2]) {
+        this.filter = "name";
+      }
+      if (value === selectOption[3]) {
+        this.filter = "servicePrice";
+      }
+    },
+    serviceOption(option) {
+      if (option === this.serviceOptions[0]) {
+        this.form.type = 1;
+      }
+      if (option === this.serviceOptions[1]) {
+        this.form.type = 2;
+      }
+    },
+    getServiceEdit(service) {
+      this.editVisible = !this.editVisible;
+      this.form.serviceID = service.serviceID;
+      this.form.type = service.type;
+      this.form.name = service.name;
+      this.form.servicePrice = service.servicePrice;
+    },
+  },
+};
 </script>
 
 <style scoped>
+.menu-bar {
+  display: flex;
+  flex-direction: row;
+  position: relative;
+}
+.menu-buttons {
+  display: flex;
+  align-items: center;
+}
+.search-container {
+  position: relative;
+  display: flex;
+}
+i {
+  color: #5f5f5f;
+}
+.fa-search {
+  position: absolute;
+  z-index: 5;
+  margin: 17px 10px;
+  font-size: 16px;
+}
+.search-field {
+  width: 180px;
+  height: 30px;
+  font-size: 16px;
+  outline: none;
+  z-index: 1;
+  padding-left: 45px;
+  border: 1px solid var(--light-grey);
+  border-radius: 50px;
+  margin-right: 20px;
+}
+.table-container {
+  display: flex;
+  height: 460px;
+}
+table {
+  width: 100%;
+  max-width: 1000;
+  border: 1px solid black;
+  border-collapse: collapse;
+  align-self: flex-start;
+  margin-top: 40px;
+  z-index: 0;
+}
+th {
+  height: 35px;
+  text-align: center;
+  background-color: #eeeeee;
+  border-bottom: 1px solid black;
+}
+td {
+  height: 35px;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+}
+.row:hover {
+  cursor: pointer;
+  background: var(--grey-highlight);
+}
+.manage {
+  height: 35px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+.fa-pencil:hover,
+.fa-trash:hover {
+  color: var(--primary-blue);
+}
+.manage-button {
+  border: none;
+  background: none;
+  cursor: pointer;
+}
+.vl {
+  border-left: 3px solid #eeeeee;
+  height: 25px;
+  margin: 0 5px;
+}
+h4 {
+  margin: 0;
+}
+select {
+  width: 150px;
+  height: 35px;
+  padding-left: 10px;
+  margin: 10px 0 20px 0;
+}
+.input-group {
+  display: flex;
+  align-items: center;
+}
+input {
+  width: 100px;
+  height: 30px;
+  padding-left: 10px;
+  margin: 10px 0 20px 0;
+}
+@media (max-width: 1100px) {
+  .search-field {
+    width: 300px;
+  }
   .menu-bar {
-    display: flex;
-    flex-direction: row;
-    position: relative;
+    flex-direction: column;
   }
   .menu-buttons {
-    display: flex;
-    align-items: center;
+    margin-top: 40px;
   }
-  .search-container {
-    position: relative;
-    display: flex;
-  }
-  i {
-    color: #5f5f5f;
-  }
-  .fa-search {
-    position: absolute;
-    z-index: 5;
-    margin: 17px 10px;
-    font-size: 16px;
-  }
+}
+@media (max-width: 700px) {
   .search-field {
     width: 180px;
-    height: 30px;
-    font-size: 16px;
-    outline: none;
-    z-index: 1;
-    padding-left: 45px;
-    border: 1px solid var(--light-grey);
-    border-radius: 50px;
-    margin-right: 20px;
   }
   .table-container {
-    display: flex;
-    height: 460px;
+    height: 450px;
   }
   table {
-    width: 100%;
-    max-width: 1000;
-    border: 1px solid black;
-    border-collapse: collapse;
-    align-self: flex-start;
-    margin-top: 40px;
-    z-index: 0;
-  }
-  th {
-    height: 35px;
-    text-align: center;
-    background-color: #eeeeee;
-    border-bottom: 1px solid black;
-  }
-  td {
-    height: 35px;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-  }
-  .row:hover {
-    cursor: pointer;
-    background: var(--grey-highlight);
-  }
-  .manage {
-    height: 35px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-  }
-  .fa-pencil:hover,
-  .fa-trash:hover {
-    color: var(--primary-blue);
-  }
-  .manage-button {
-    border: none;
-    background: none;
-    cursor: pointer;
+    margin-top: 30px;
+    font-size: 14px;
   }
   .vl {
-    border-left: 3px solid #eeeeee;
-    height: 25px;
-    margin: 0 5px;
+    margin: 0 2px;
   }
-  h4 {
-    margin: 0;
+  .fa-pencil,
+  .fa-trash {
+    font-size: 20px;
   }
-  select {
-    width: 150px;
-    height: 35px;
-    padding-left: 10px;
-    margin: 10px 0 20px 0;
-  }
-  .input-group {
-    display: flex;
-    align-items: center;
-  }
-  input {
-    width: 100px;
-    height: 30px;
-    padding-left: 10px;
-    margin: 10px 0 20px 0;
-  }
-  @media (max-width: 1100px) {
-    .search-field {
-      width: 300px;
-    }
-    .menu-bar {
-      flex-direction: column;
-    }
-    .menu-buttons {
-      margin-top: 40px;
-    }
-  }
-  @media (max-width: 700px) {
-    .search-field {
-      width: 180px;
-    }
-    .table-container {
-      height: 450px;
-    }
-    table {
-      margin-top: 30px;
-      font-size: 14px;
-    }
-    .vl {
-      margin: 0 2px;
-    }
-    .fa-pencil,
-    .fa-trash {
-      font-size: 20px;
-    }
-  }
+}
 </style>
