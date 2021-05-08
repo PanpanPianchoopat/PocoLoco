5bc4d6c69d32e03fad47e97c11ee121cfa285d3d
PanpanPianchoopat <62735973+PanpanPianchoopat@users.noreply.github.com>
2021-05-08 15:38:27 +0700

    Multilple page pagination

--- src/components/PaginationBar.vue
+++ src/components/PaginationBar.vue
@@ -1,27 +1,46 @@
 <template>
   <div class="pagination-container" v-if="paginationVisible">
     <div class="pagination-row">
+      <button
+        class="pagination-button"
+        @click="prevGroup"
+        v-if="doubleArrowVisible"
+      >
+        <i class="fa fa-angle-double-left"></i>
+      </button>
       <button class="pagination-button" @click="prev">
         <i class="fa fa-chevron-left fa-1x"></i>
       </button>
-      <span v-for="(item, i) in new Array(pageCount)" :key="i">
+      <span
+        v-for="(page, i) in pageNumber.slice(
+          10 * pageGroup,
+          10 * pageGroup + 10
+        )"
+        :key="i"
+      >
         <button
-          @click="setCurrentPage(i + 1)"
+          @click="setCurrentPage(page)"
           class="pagination-button"
           v-bind:style="
-            currentPage === i + 1
+            currentPage === page
               ? { background: 'var(--primary-blue)', color: 'white' }
               : {}
           "
         >
-          {{ i + 1 }}
+          {{ page }}
         </button>
       </span>
       <button class="pagination-button" @click="next">
         <i class="fa fa-chevron-right fa-1x"></i>
       </button>
+      <button
+        class="pagination-button"
+        @click="nextGroup"
+        v-if="doubleArrowVisible"
+      >
+        <i class="fa fa-angle-double-right"></i>
+      </button>
     </div>
-    <slot />
   </div>
 </template>
 
@@ -32,19 +51,51 @@
     data() {
       return {
         currentPage: 1,
+        pageNumber: [],
+        pageGroup: 0,
+        doubleArrowVisible: false,
       };
     },
+    created() {
+      this.pageNumber = Array.from(new Array(this.pageCount), (x, i) => i + 1);
+      if (this.pageCount / 10 > 1) {
+        this.doubleArrowVisible = true;
+      }
+    },
     methods: {
+      setPageGroup(current) {
+        var i = 0;
+        while (i < 20) {
+          if (current > i * 10 && current <= i * 10 + 10) {
+            return i;
+          }
+          i = i + 1;
+        }
+      },
       prev() {
         if (this.currentPage > 1) {
           this.currentPage = this.currentPage - 1;
+          this.pageGroup = this.setPageGroup(this.currentPage);
           this.$emit("pageReturn", this.currentPage);
         }
       },
       next() {
         if (this.currentPage != this.pageCount) {
           this.currentPage = this.currentPage + 1;
+          this.pageGroup = this.setPageGroup(this.currentPage);
           this.$emit("pageReturn", this.currentPage);
+        }
+      },
+      prevGroup() {
+        if (this.pageGroup > 0) {
+          this.pageGroup -= 1;
+          this.currentPage = this.pageGroup * 10 + 1;
+        }
+      },
+      nextGroup() {
+        if (this.pageGroup < Math.ceil(this.pageCount / 10) - 1) {
+          this.pageGroup += 1;
+          this.currentPage = this.pageGroup * 10 + 1;
         }
       },
       setCurrentPage(page) {
@@ -79,6 +130,11 @@
   }
   i {
     color: var(--primary-blue);
+  }
+  .fa-angle-double-left,
+  .fa-angle-double-right {
+    font-size: 18px;
+    font-weight: bold;
   }
   *:focus {
     outline: 0;
